e768fd36a13cead7b931b4cb138935ef4f2181fd
bryansashakim <Bryankim776@gmail.com>
2020-08-26 20:34:01 -0700

    First commit

---
+++ sapply practice.R
@@ -0,0 +1,10 @@
+test_vec=c("R","E","E","S","I","R")
+t=which(test_vec=="E")
+
+test_vec2=temp_action_Time
+
+sapply(test_vec2, function(x) x*-1)
+
+Frac_test=rep(0,20)
+
+sapply(Frac_test, function(x) x*-1)
